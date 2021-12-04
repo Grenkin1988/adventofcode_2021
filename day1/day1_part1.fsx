a468ca35d07d7ffa059dcc1dce5055542bb4d155
Grenkin1988 <kruglovgleb@gmail.com>
2021-12-04 18:36:16 +0100

    Day 1 is done

---
+++ day1/day1_part1.fsx
@@ -0,0 +1,14 @@
+open System
+
+let readLines filePath = System.IO.File.ReadLines(filePath)
+let lines = readLines (__SOURCE_DIRECTORY__ + "/day1_input.csv")
+
+let numbers = lines |> Seq.map Int32.Parse
+
+let result =
+    numbers
+    |> Seq.pairwise
+    |> Seq.map (fun (i,j) -> j > i)
+    |> Seq.sumBy System.Convert.ToInt32
+
+Console.WriteLine(result)
