7ab53a2f33905222974c5da9be980d7afc088d69
Grenkin1988 <kruglovgleb@gmail.com>
2021-12-07 10:25:10 +0100

    Day 7 is done

--- AdventOfCode2021/src/FSharp/Day7/Program.fs
+++ AdventOfCode2021/src/FSharp/Day7/Program.fs
@@ -2,5 +2,24 @@
 let fileName = sprintf "%s/%s" __SOURCE_DIRECTORY__ "/day7_input.txt"
 let lines = Helpers.readLines fileName
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+let numbers = lines.Head.Split(",", StringSplitOptions.TrimEntries) |> Array.map int |> List.ofArray
+
+let part1 =
+    numbers
+    |> List.map (fun n -> n, numbers |> List.map (fun m -> abs (m - n)) |> List.sum)
+    |> List.minBy snd
+
+printfn "%A" part1
+
+let fuelTo from destination =
+    match abs(from - destination) with
+    | 0 -> 0
+    | 1 -> 1
+    | n -> [1..n] |> List.sum
+
+let part2 =
+    [numbers |> List.min .. numbers |> List.max]
+    |> List.map (fun n -> n, numbers |> List.map (fun m -> fuelTo m  n) |> List.sum)
+    |> List.minBy snd
+
+printfn "%A" part2
