612f6a709d7f4fe7c60778c87804a627e620c19d
Grenkin1988 <kruglovgleb@gmail.com>
2021-12-08 21:30:38 +0100

    Prepare

---
+++ AdventOfCode2021/src/FSharp/Day9/Program.fs
@@ -0,0 +1,6 @@
+﻿open System
+let fileName = sprintf "%s/%s" __SOURCE_DIRECTORY__ "/day9_testinput.txt"
+let lines = Helpers.readLines fileName
+
+// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
